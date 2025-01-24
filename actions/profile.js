0cd5286402a8dde944e11cb7d4285bb370c33f8d
mohamadkzd <kazemzademohammad4@gmail.com>
2025-01-24 13:43:07 +0330

    update profile and createForm

--- actions/profile.js
+++ actions/profile.js
@@ -31,7 +31,7 @@ async function editInfo(stateOtp, formData) {
       name,
       email,
     },
-    { Authorization: `Bearer ${token.value}` }
+    { 'Authorization': `Bearer ${token.value}` }
   );
   // console.log("testData", data);
   // vaghti data dorost post shavad samte server va status 200 bar gardone responsi ke bar migardone login token hast ke yek meghdari dare be sorat strin login_token:"fdsfsdf"
@@ -49,4 +49,86 @@ async function editInfo(stateOtp, formData) {
   }
 }
 
-export { editInfo };
+async function createAddress(stateOtp, formData) {
+  // console.log("test",formData);
+
+  const title = formData.get("title");
+  const cellphone = formData.get("cellphone");
+  const postal_code = formData.get("postal_code");
+  const province_id = formData.get("province_id");
+  const city_id = formData.get("city_id");
+  const address = formData.get("address");
+
+  if (title === "") {
+    return {
+      status: "error",
+      message: "فیلد عنوان الزامی است",
+    };
+  }
+
+  const cellphonePattern = /^(\+98|0)?9\d{9}$/;
+  if (cellphone === "" || !cellphonePattern.test(cellphone)) {
+    return {
+      status: "error",
+      message: "فیلد شماره تماس نامعتبر است",
+    };
+  }
+
+  const postalCodePattern = /^\d{5}[ -]?\d{5}$/i;
+  if (postal_code === "" || !postalCodePattern.test(postal_code)) {
+    return {
+      status: "error",
+      message: "فیلده کده پستی نامعتبر است",
+    };
+  }
+
+  // if (province_id === "") {
+  //   return {
+  //     status: "error",
+  //     message: "فیلد ایمیل الزامی است",
+  //   };
+  // }
+
+  // if (city_id === "") {
+  //   return {
+  //     status: "error",
+  //     message: "فیلد ایمیل الزامی است",
+  //   };
+  // }
+  if (address === "") {
+    return {
+      status: "error",
+      message: "فیلده آدرس الزامی است",
+    };
+  }
+
+  const token = cookies().get("token");
+
+  const data = await postFetch(
+    "/profile/addresses/create",
+    {
+      title,
+      cellphone,
+      postal_code,
+      province_id,
+      city_id,
+      address,
+    },
+    { 'Authorization': `Bearer ${token.value}` }
+  );
+
+  if (data.status === "success") {
+    return {
+      status: data.status,
+      message: "ثبت آدرس با موفقیت انجام شد",
+      user: data.data.user,
+    };
+  } else {
+    return {
+      status: data.status,
+      message: handelError(data.message),
+    };
+  }
+}
+
+export { editInfo, createAddress };
--- src/components/(profile)/addresess/createForm/createForm.js
+++ src/components/(profile)/addresess/createForm/createForm.js
@@ -1,11 +1,20 @@
 "use client";
 
-import { useState } from "react";
-
+import SubmitBtn from "@/ui/submitBtn/submitBtn";
+import { useEffect,useState } from "react";
+import { useFormState } from "react-dom";
+import { toast } from "react-toastify";
+import { createAddress } from "../../../../../actions/profile";
 const CreateForm = ({ provinces, cities }) => {
-  const [citiesFilter, setCitiesFilter] = useState(cities.filter((city) => city.province_id == provinces[0].id));
+  const [citiesFilter, setCitiesFilter] = useState(
+    cities.filter((city) => city.province_id == provinces[0].id)
+  );// ma mitonim dakhele state ro arraye khali bzarim , vali age data e filter shode ke ostan haye meghdar aval id on haro neshon bde omadim az az filter estefade krdim
 
-// ma mitonim dakhele state ro arraye khali bzarim , vali age data e filter shode ke ostan haye meghdar aval id on haro neshon bde omadim az az filter estefade krdim
+  const [stateCreate,fotmActionCreate]=useFormState(createAddress,{})
+
+  useEffect(()=>{
+      toast(stateCreate?.message,{type :`${stateCreate?.status}`})
+  },[stateCreate])
 
   const chnageProvince = (e) => {
     // console.log(cities.filter((city) => city.province_id == e.target.value));
@@ -15,7 +24,7 @@ const CreateForm = ({ provinces, cities }) => {
     );
   };
   return (
-    <form>
+    <>
       <button
         className="btn btn-primary"
         type="button"
@@ -25,24 +34,24 @@ const CreateForm = ({ provinces, cities }) => {
         ایجاد آدرس جدید
       </button>
 
-      <div className="collapse mt-3" id="collapseExample">
+      <form action={fotmActionCreate} className="collapse mt-3" id="collapseExample">
         <div className="card card-body">
           <div className="row g-4">
             <div className="col col-md-6">
               <label className="form-label">عنوان</label>
-              <input type="text" className="form-control" />
+              <input name="title" type="text" className="form-control" />
             </div>
             <div className="col col-md-6">
               <label className="form-label">شماره تماس</label>
-              <input type="text" className="form-control" />
+              <input name="cellphone" type="text" className="form-control" />
             </div>
             <div className="col col-md-6">
               <label className="form-label">کد پستی</label>
-              <input type="text" className="form-control" />
+              <input name="postal_code" type="text" className="form-control" />
             </div>
             <div className="col col-md-6">
               <label className="form-label">استان</label>
-              <select className="form-select" onChange={chnageProvince}>
+              <select name="province_id" className="form-select" onChange={chnageProvince}>
                 {provinces?.map((province) => {
                   return (
                     <option key={province.id} value={province.id}>
@@ -54,7 +63,7 @@ const CreateForm = ({ provinces, cities }) => {
             </div>
             <div className="col col-md-6">
               <label className="form-label">شهر</label>
-              <select className="form-select">
+              <select name="city_id" className="form-select">
                 {citiesFilter.map((city) => {
                   return (
                     <option key={city.id} value={city.id}>
@@ -67,6 +76,7 @@ const CreateForm = ({ provinces, cities }) => {
             <div className="col col-md-12">
               <label className="form-label">آدرس</label>
               <textarea
+              name="address"
                 type="text"
                 rows="5"
                 className="form-control"
@@ -74,11 +84,11 @@ const CreateForm = ({ provinces, cities }) => {
             </div>
           </div>
           <div>
-            <button className="btn btn-primary mt-4">ایجاد</button>
+            <SubmitBtn title="ایجاد" style="btn btn-primary mt-4"/>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
