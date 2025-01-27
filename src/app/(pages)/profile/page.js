973b6c9946693c30054dabe491c62c11158594dc
mohamadkzd <kazemzademohammad4@gmail.com>
2025-01-27 12:34:11 +0330

    update middlware

--- src/app/(pages)/profile/page.js
+++ src/app/(pages)/profile/page.js
@@ -2,14 +2,16 @@ import EditForm from "@/components/(profile)/Info/EditForm/editForm";
 import { getFetch } from "@/lib/config/fetcher";
 import { cookies } from "next/headers";
 
-const Profile = async() => {
-    const token = cookies().get('token')
-   const user=await getFetch("/profile/info",{'Authorization':`Bearer ${token.value}`})
-//    console.log(user);
-   
+const Profile = async () => {
+  const token = cookies().get("token");
+  const user = await getFetch("/profile/info", {
+    Authorization: `Bearer ${token.value}`,
+  });
+  //    console.log(user);
+
   return (
     <div className="vh-70">
-       <EditForm user={user}/>
+      <EditForm user={user} />
     </div>
   );
 };
